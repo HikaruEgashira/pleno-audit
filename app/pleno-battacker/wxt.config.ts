46f407e5e5421b75bb6feb87191a05f5a6314034
HikaruEgashira <account@egahika.dev>
2026-01-17 00:27:04 +0900

    feat: add battacker logo with cyberpunk crosshair design

--- app/pleno-battacker/wxt.config.ts
+++ app/pleno-battacker/wxt.config.ts
@@ -14,6 +14,12 @@ export default defineConfig({
     name: "Pleno Battacker",
     version: "1.0.0",
     description: "Browser Defense Resistance Testing Tool - Simulates attack patterns to evaluate browser security",
+    icons: {
+      16: "icon-16.png",
+      32: "icon-32.png",
+      48: "icon-48.png",
+      128: "icon-128.png",
+    },
     permissions: [
       "storage",
       "alarms",
